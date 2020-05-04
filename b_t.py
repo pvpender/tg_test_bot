97a41ddb6baf37a3f2db04e05d4cba4d2942fc59
pvpender <59583646+pvpender@users.noreply.github.com>
2020-05-04 14:57:34 +0300

    Add files via upload

---
+++ b_t.py
@@ -0,0 +1,15 @@
+import aiogram as ai
+import logging
+import random
+TOKEN='1133381423:AAEytfr8xb5xoB9iewgDWPAwKZlMgkArW_w'
+
+
+logging.basicConfig(level=logging.INFO )
+bot=ai.Bot(token=TOKEN)
+dp=ai.Dispatcher(bot )
+@dp.message_handler(commands=['start'])
+async def send_mes(message: ai.types.message):
+    await message.answer("Привет! Тебе")
+
+if __name__ =='__main__':
+    ai.executor.start_polling(dp, skip_updates= True )
