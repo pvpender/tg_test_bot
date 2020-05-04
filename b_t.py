b7729a7efabbd9138f52c950618ffd81abb33eb5
pvpender <59583646+pvpender@users.noreply.github.com>
2020-05-04 15:44:08 +0300

    Update b_t.py

--- b_t.py
+++ b_t.py
@@ -10,7 +10,7 @@ bot=ai.Bot(token=TOKEN)
 dp=ai.Dispatcher(bot )
 @dp.message_handler(commands=['start'])
 async def send_mes(message: ai.types.message):
-    text="Привет тебе"+ str(random.randint(1,100))+" ?"
+    text="Привет тебе "+ str(random.randint(1,100))+"?"
     await message.answer(text)
   
 
