fdf9e8c860f441ba52e563ccb1a1d05a651043a1
pvpender <59583646+pvpender@users.noreply.github.com>
2020-05-04 15:28:49 +0300

    Update b_t.py

--- b_t.py
+++ b_t.py
@@ -2,10 +2,7 @@ import aiogram as ai
 import logging
 import random
 TOKEN='1133381423:AAEytfr8xb5xoB9iewgDWPAwKZlMgkArW_w'
-def rand():
-    a=random.randint(1,100)
-    time.sleep(0.5)
-    rand()
+
 
 
 logging.basicConfig(level=logging.INFO )
@@ -13,11 +10,11 @@ bot=ai.Bot(token=TOKEN)
 dp=ai.Dispatcher(bot )
 @dp.message_handler(commands=['start'])
 async def send_mes(message: ai.types.message):
-    await message.answer(a)
-    a=random.randint(1,100)
+    await message.answer("Привет, тебе", str(random.randint(1,100)),"?")
+  
 
 
 if __name__ =='__main__':
    
     ai.executor.start_polling(dp, skip_updates= True )
-    rand()
+
