ff87443d7d6304f79924d37b8414aa45aed02d34
pvpender <59583646+pvpender@users.noreply.github.com>
2020-05-05 14:09:21 +0300

    Add files via upload

--- smt.py
+++ smt.py
@@ -60,15 +60,15 @@ async def check(message: types.message ):
 async def check(message: types.message ):
     a=random.randint(-20,40)
     if (a<0):
-        id = '@' + str(message.forward_from)
+        id = '@' + str(message.forward_from.username)
         text = "У "+id+" Неправильна писка, всего " + str(a) + " мм!"
         await message.answer(text)
     if(a==0):
-        id = '@' + str(message.forward_from)
+        id = '@' + str(message.forward_from.username)
         text = "У "+id+ " не ма писка"
         await message.answer(text)
     if(a>0):
-        id = '@' + str(message.forward_from)
+        id = '@' + str(message.forward_from.username)
         text = "У "+id+ " писка "+str(a)+" мм!"
         await message.answer(text)
 
