949a0ced8cd71034dffa6b60eb0faa8e49324b66
pvpender <59583646+pvpender@users.noreply.github.com>
2020-05-05 11:29:12 +0300

    H

--- smt.py
+++ smt.py
@@ -36,7 +36,8 @@ async def echo(message: types.Message):
             await message.answer(text)'''
     if (a>0):
         text="Твой писка "+str(a)+" мм!"
-        id='@'+str(message.from_user.username)
+        id1=message.reply_to_message.from_user.username
+        id='@'+str(id1)
         text=id+' '+text
         await message.answer(text)
 
